869e4bd513e5e67cb7177b21852352922dabaf03
Jessica Deen <jedeen@outlook.com>
2021-09-09 00:41:06 -0700

    Update Generate-Config.ps1

--- Deploy/powershell/Generate-Config.ps1
+++ Deploy/powershell/Generate-Config.ps1
@@ -69,13 +69,10 @@ else {
 $appinsightsId=@()
 $appInsightsName=$(az resource list -g $resourceGroup --resource-type Microsoft.Insights/components --query [].name | ConvertFrom-Json)
 if ($appInsightsName -and $appInsightsName.Length -eq 1) {
-    $appinsightsConfig=$(az monitor app-insights component show --app $appInsightsName -g $resourceGroup -o json | ConvertFrom-Json)
-
-    if ($appinsightsConfig) {
-        $appinsightsId = $appinsightsConfig.instrumentationKey           
-    }
+    $appinsightsId = $(az monitor app-insights component show --app $appInsightsName -g $resourceGroup --query instrumentationKey -o json | ConvertFrom-Json)
 }
 Write-Host "App Insights Instrumentation Key: $appinsightsId" -ForegroundColor Yellow
+
 
 ## Showing Values that will be used
 
